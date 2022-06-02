99b289af3eaaf112ec5558a1a364473706d171a8
lijunhao <lijunhaoit@163.com>
2022-06-02 13:16:04 +0800

    add
    add

---
+++ src/main/java/cn/lijunhaoit/myblog/service/FileService.java
@@ -0,0 +1,20 @@
+package cn.lijunhaoit.myblog.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.Future;
+import java.util.concurrent.locks.ReentrantLock;
+
+@Service
+public class FileService {
+
+    private Logger logger = LoggerFactory.getLogger(FileService.class);
+
+//    public Future<Boolean> kkk(){
+//        final ReentrantLock lock = new ReentrantLock();
+//        lock.lock();
+//        return
+//    }
+}
--- src/main/java/cn/lijunhaoit/myblog/service/UserService.java
+++ src/main/java/cn/lijunhaoit/myblog/service/UserService.java
@@ -3,16 +3,21 @@ package cn.lijunhaoit.myblog.service;
 
 import cn.lijunhaoit.myblog.entity.User;
 import cn.lijunhaoit.myblog.mapper.UserMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Random;
 
 @Service
 public class UserService {
 
+    private Logger logger = LoggerFactory.getLogger(UserService.class);
     @Autowired
     private UserMapper userMapper;
 
@@ -26,5 +31,19 @@ public class UserService {
 
     public void evictUser(Long id) {
         System.out.println("evict user:" + id);
+    }
+
+    @Async("asyncExecutor")
+    public void executeAsync(){
+        int id = new Random().nextInt(100);
+
+        logger.info("{} start",id);
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        logger.info("{} end",id);
     }
 }
